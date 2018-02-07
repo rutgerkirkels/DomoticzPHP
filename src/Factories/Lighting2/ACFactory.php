cf059c5353130afb4760a358df9bedc48ac396e0
Rutger Kirkels <rutger@kirkels.nl>
2018-02-07 23:44:25 +0100

    Removed unused Light class. Code cleanup

--- src/Factories/Lighting2/ACFactory.php
+++ src/Factories/Lighting2/ACFactory.php
@@ -1,16 +1,25 @@
 <?php
 
-
 namespace rutgerkirkels\DomoticzPHP\Factories\Lighting2;
-
 
 use rutgerkirkels\DomoticzPHP\Devices\Switches\OnOff;
 use rutgerkirkels\DomoticzPHP\Factories\AbstractDeviceFactory;
 
+/**
+ * Class ACFactory
+ * @package rutgerkirkels\DomoticzPHP\Factories\Lighting2
+ */
 class ACFactory extends AbstractDeviceFactory
 {
+    /**
+     * @var OnOff
+     */
     public $data;
 
+    /**
+     * ACFactory constructor.
+     * @param object $deviceData
+     */
     public function __construct(object $deviceData)
     {
         switch ($deviceData->SwitchType) {
